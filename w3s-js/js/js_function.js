f069f8345f2114955ff0aa03e0aeb99c00695f1c
vhtuananh020402 <vhtuananh020402@gmail.com>
2024-09-04 01:33:29 +0700

    js function

---
+++ w3s-js/js/js_function.js
@@ -0,0 +1,19 @@
+function summation(num1, num2) {
+    return num1 + num2;
+}
+
+let num1 = 12;
+let num2 = 15;
+let result = summation(num1, num2);
+
+
+console.log(result);
+
+
+
+function toCelsius(fahrenheit) {
+    return (5/9) * (fahrenheit-32);
+}
+
+let value = toCelsius;
+console.log(value);

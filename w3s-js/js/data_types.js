d3bee3e5b94d2dd016483790bf928fb7ca83320a
vhtuananh020402 <vhtuananh020402@gmail.com>
2024-09-04 01:28:26 +0700

    js data types

---
+++ w3s-js/js/data_types.js
@@ -0,0 +1,22 @@
+// 8 data types
+let _string = "This is a string";
+let _number = 12;
+let _bigInt = BigInt("10921030129301293091239012382130213821380213902193");
+let _boolean = true;
+let _undefined = undefined; // or let _undefined;
+let _null = null;
+let _symbol = Symbol("foo");
+
+// Objects
+/*
+Built-in object types can be:
+
+objects, arrays, dates, maps, sets, intarrays, floatarrays, promises, and more.
+*/
+const person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"}; // object
+
+
+console.log(_bigInt);
+console.log(_undefined);
+
+console.log(typeof(person));
